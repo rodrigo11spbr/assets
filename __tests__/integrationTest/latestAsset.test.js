f068e1793c3af2d6f3db0bdd6b8a0357902b8b03
Rodrigo Prado <rodrigo11spbr@outlook.com>
2020-05-18 11:44:57 -0300

    finishing test

--- __tests__/integrationTest/latestAsset.test.js
+++ __tests__/integrationTest/latestAsset.test.js
@@ -2,13 +2,15 @@ const server = require('../../src/server');
 const request = require("supertest")(server);
 
 describe('when not send a valid ticker', () => {
-    test('should return status code 400', () => {
-        request.post('/api/asset/').send({ tickers: [] }).expect(400)
+    test('should return status code 400', (done) => {
+        request.post('/api/asset/').send({ tickers: [] }).expect(400);
+        done();
     });
 });
 
 describe('when send a valid tickers', () => {
-    test('should return status code 200 with body', () => {
+    test('should return status code 200 with body', (done) => {
         request.post('/api/asset').send({ tickers: ['msft'] }).expect(200);
+        done();
     });
 });
